5598f85a47656a1caae97b67022814ef5eac548e
ABiatov <anton.biatov@gmail.com>
2016-03-28 22:42:29 +0300

    Загрузил главную чтраницу и страницы с 3Д моделями. В базовую страницу встроил модели при помощи iframe. Создал всплывающее окно для для цитирования(требует доработки).

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
